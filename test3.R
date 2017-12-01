eb20e35477c59afbfeb9b53178a9da2a957de317
penghangliu <penghangliu@gmail.com>
2017-11-30 23:26:30 -0500

    second draft

--- test3.R
+++ test3.R
@@ -42,20 +42,136 @@ data_merge <- geo_join(ny, data, "NAME_2", "county") %>% st_as_sf()
 datatable(data)
 names(ny)
 
-data_merge
+datatable(data)
 
 st_transform(data_merge,"+proj=longlat +datum=WGS84")%>%
   leaflet() %>% addTiles() %>%
-  addPolygons(label=paste(data_merge$county," (index=",data_merge$index_rate,")"),
-              group = "index",
-              color = "#444444", 
-              weight = 0.1, 
+  addPolygons(label=paste(data_merge$county," (Crime Rates=",data_merge$index_rate,")"),
+              group = "All Crimess",
+              color = "white", 
+              dashArray = "3",
+              weight = 1, 
               smoothFactor = 0.5,
               opacity = 1.0, 
               fillOpacity = 0.5,
               fillColor = ~colorQuantile("YlOrRd", index_rate)(index_rate),
-              highlightOptions = highlightOptions(color = "white", weight = 2,
+              highlightOptions = highlightOptions(color = "white", weight = 3,
                                                   bringToFront = TRUE))%>%
+  addPolygons(label=paste(data_merge$county," (Crime Rates=",data_merge$violent_rate,")"),
+              group = "Violent Crimes",
+              color = "white", 
+              dashArray = "3",
+              weight = 1, 
+              smoothFactor = 0.5,
+              opacity = 1.0, 
+              fillOpacity = 0.5,
+              fillColor = ~colorQuantile("YlOrRd", violent_rate)(violent_rate),
+              highlightOptions = highlightOptions(color = "white", weight = 3,
+                                                  bringToFront = TRUE))%>%
+  addPolygons(label=paste(data_merge$county," (Crime Rates=",data_merge$firearm_rate,")"),
+              group = "Firearm Crimes",
+              color = "white", 
+              dashArray = "3",
+              weight = 1, 
+              smoothFactor = 0.5,
+              opacity = 1.0, 
+              fillOpacity = 0.5,
+              fillColor = ~colorQuantile("YlOrRd", firearm_rate)(firearm_rate),
+              highlightOptions = highlightOptions(color = "white", weight = 3,
+                                                  bringToFront = TRUE))%>%
+  addPolygons(label=paste(data_merge$county," (Crime Rates=",data_merge$property_rate,")"),
+              group = "Property Crimes",
+              color = "white", 
+              dashArray = "3",
+              weight = 1, 
+              smoothFactor = 0.5,
+              opacity = 1.0, 
+              fillOpacity = 0.5,
+              fillColor = ~colorQuantile("YlOrRd", property_rate)(property_rate),
+              highlightOptions = highlightOptions(color = "white", weight = 3,
+                                                  bringToFront = TRUE))%>%
+  addPolygons(label=paste(data_merge$county," (Gini Index=",data_merge$Gini_Index,")"),
+              group = "Income Inequality",
+              color = "white", 
+              dashArray = "3",
+              weight = 1, 
+              smoothFactor = 0.5,
+              opacity = 1.0, 
+              fillOpacity = 0.5,
+              fillColor = ~colorQuantile("YlOrRd", Gini_Index)(Gini_Index),
+              highlightOptions = highlightOptions(color = "white", weight = 3,
+                                                  bringToFront = TRUE))%>%
+  addPolygons(label=paste(data_merge$county," (Under Highschool=",data_merge$under_highschool,"%)"),
+              group = "Education Attainment",
+              color = "white", 
+              dashArray = "3",
+              weight = 1, 
+              smoothFactor = 0.5,
+              opacity = 1.0, 
+              fillOpacity = 0.5,
+              fillColor = ~colorQuantile("YlOrRd", under_highschool)(under_highschool),
+              highlightOptions = highlightOptions(color = "white", weight = 3,
+                                                  bringToFront = TRUE))%>%
+  addPolygons(label=paste(data_merge$county," (Unemployment Rates=",100*data_merge$unemployment,"%)"),
+              group = "Unemployment Rates",
+              color = "white", 
+              dashArray = "3",
+              weight = 1, 
+              smoothFactor = 0.5,
+              opacity = 1.0, 
+              fillOpacity = 0.5,
+              fillColor = ~colorQuantile("YlOrRd", unemployment)(unemployment),
+              highlightOptions = highlightOptions(color = "white", weight = 3,
+                                                  bringToFront = TRUE))%>%
+  addLayersControl(
+    baseGroups = c("All Crimes", "Violent Crimes", "Firearm Crimes", "Property Crimes", "Income Inequality","Education Attainment","Unemployment Rates"),
+    options = layersControlOptions(collapsed = FALSE)
+  )%>%
   addMiniMap()%>%
   frameWidget(height = 600)
 
+##########################################################
+
+data_merge=mutate(data_merge,Gini_index=as.numeric(scale(Gini_Index)))
+data_merge=mutate(data_merge,Under_highschool=as.numeric(scale(under_highschool)))
+data_merge=mutate(data_merge,Unemployment=as.numeric(scale(unemployment)))
+
+GLM <- glm(index_rate~Gini_index, data=data_merge, family="gaussian")
+MRM <- lm(index_rate~Gini_index + Under_highschool + Unemployment, data = data_merge)
+GAM <- gam(index_rate~Gini_index + Under_highschool + Unemployment, data = data_merge, family = "gaussian")
+
+All <- gam(index_rate~Gini_index + Under_highschool + Unemployment, data = data_merge, family = "gaussian")
+Violent <- gam(violent_rate~Gini_index + Under_highschool + Unemployment, data = data_merge, family = "gaussian")
+Firearm <- gam(firearm_rate~Gini_index + Under_highschool + Unemployment, data = data_merge, family = "gaussian")
+Property <- gam(property_rate~Gini_index + Under_highschool + Unemployment, data = data_merge, family = "gaussian")
+
+summary(GLM)
+summary(MRM)
+summary(GAM)
+
+xtable(GLM,
+       caption="Summary for Generalized Linear Model")%>%
+  print(type="html")
+
+xtable(MRM,
+       caption="Summary for Multiple Regression Model")%>%
+  print(type="html")
+
+xtable(summary(All)$p.table,
+       caption="Summary for All Crimes Model")%>%
+  print(type="html")
+
+xtable(summary(Violent)$p.table,
+       caption="Summary for Violent Crimes Model")%>%
+  print(type="html")
+
+xtable(summary(Firearm)$p.table,
+       caption="Summary for Firearm Crimes Model")%>%
+  print(type="html")
+
+xtable(summary(Property)$p.table,
+       caption="Summary for Property Crimes Model")%>%
+  print(type="html")
+
+vif(All)
+datatable(AIC(INX,VIO,FIR,POR))
